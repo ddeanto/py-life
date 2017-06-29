1eb1dd883e0819d82928f9050752aac98d680926
dsynk <danny.synk@gmail.com>
2017-06-29 19:50:36 -0400

    Dev folder for year/month list generator; integration with terminal_escrape

--- sandbox/escrape_button.py
+++ sandbox/escrape_button.py
@@ -6,23 +6,27 @@ import requests
 from bs4 import BeautifulSoup
 
 base_url = 'http://terminalescape.blogspot.com/'
-years = ['2015', '2016']
-months = ['01', '02', '03', '04', '05', '06', '07', '09', '10', '11', '12']
+years = ['2013', '2014', '2015', '2016', '2017']
+months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+beginYear = 2013
+beginMonth = 5
+endYear = 2017
+endMonth = 6
 
-urls = []
-for year in years:
-    for month in months:
-        url = base_url + year + '/' + month
-        urls.append(url)
+#urls = []
+dates = []
+for year in range(2013, 2017+1):
+    for month in range(1,12+1):
+        if year == beginYear and month < beginMonth:
+            pass
+        elif year == endYear and month > endMonth:
+            pass 
+        else: 
+            dates.append([str(year), months[month-1]])
 
-links = []
-for url in urls:
-    print (url)
-    response = requests.get(url)
-    html = response.text
-    soup = BeautifulSoup(html, "html.parser")
+print(dates)
 
-    a_tag_matches = soup.select('h3.post-title.entry-title > a[href]')
-    links += [a['href'] for a in a_tag_matches]
+#a_tag_matches = soup.select('h3.post-title.entry-title > a[href]')
+#links += [a['href'] for a in a_tag_matches]
 
-print (links)
+#print (link)
--- terminal_escrape.py
+++ terminal_escrape.py
@@ -3,17 +3,30 @@ from bs4 import BeautifulSoup
 
 base_url = 'http://terminalescape.blogspot.com/'
 years = ['2015', '2016']
-months = ['01', '02', '03', '04', '05', '06', '07', '09', '10', '11', '12']
+months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+beginYear = 2013
+beginMonth = 5
+endYear = 2017
+endMonth = 6
+
+dates = []
+for year in range(2013, 2017+1):
+    for month in range(1,12+1):
+        if year == beginYear and month < beginMonth:
+            pass
+        elif year == endYear and month > endMonth:
+            pass 
+        else: 
+            dates.append([str(year), months[month-1]])
 
 urls = []
-for year in years:
-    for month in months:
-        url = base_url + year + '/' + month
+for post in dates:
+        url = base_url + post[0] + '/' + post[1]
         urls.append(url)
 
 links = []
 for url in urls:
-    print url
+    print (url)
     response = requests.get(url)
     html = response.text
     soup = BeautifulSoup(html, "html.parser")
@@ -21,4 +34,4 @@ for url in urls:
     a_tag_matches = soup.select('h3.post-title.entry-title > a[href]')
     links += [a['href'] for a in a_tag_matches]
 
-print links
+print (links)
