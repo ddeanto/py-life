e496b87b99af6e839b4338dad0d84a3878b94a56
Darren Deanto <darrendgeanto@gmail.com>
2017-06-29 23:46:10 -0400

    takes cmd line args

--- terminal_escrape.py
+++ terminal_escrape.py
@@ -1,16 +1,22 @@
+# run this in cmd line python terminal_escrape.py 2014 5 2017 6
+
+import sys # so we can access cmd line args
 import requests
 from bs4 import BeautifulSoup
 
+# print cmd line arguments - it's a list
+print(sys.argv)
+# sys.argv[0] is the name of the script and sys.argv[1], sys.argv[2], ... sys.argv[n] get the args
+beginYear = int(sys.argv[1])
+beginMonth = int(sys.argv[2])
+endYear = int(sys.argv[3])
+endMonth = int(sys.argv[4])
+
 base_url = 'http://terminalescape.blogspot.com/'
-years = ['2015', '2016']
 months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
-beginYear = 2013
-beginMonth = 5
-endYear = 2017
-endMonth = 6
 
 dates = []
-for year in range(2013, 2017+1):
+for year in range(beginYear, endYear+1):
     for month in range(1,12+1):
         if year == beginYear and month < beginMonth:
             pass
