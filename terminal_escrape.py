1011d2a1fe98868e47552f3b6810f393e3df31d8
Darren Deanto <darrengdeanto@gmail.com>
2017-07-07 02:01:27 -0400

    mvp working i think

--- terminal_escrape.py
+++ terminal_escrape.py
@@ -3,6 +3,14 @@
 import sys # so we can access cmd line args
 import requests
 from bs4 import BeautifulSoup
+import urllib
+import re
+import os
+
+def downloadIt(url, path):
+    print('downloading: ' + url)
+    data = urllib.request.urlretrieve(url, path)
+    print('saved to: ' + path)
 
 # print cmd line arguments - it's a list
 print(sys.argv)
@@ -12,6 +20,8 @@ beginMonth = int(sys.argv[2])
 endYear = int(sys.argv[3])
 endMonth = int(sys.argv[4])
 
+pwd = os.getcwd()
+print 'pwd: ' + pwd
 base_url = 'http://terminalescape.blogspot.com/'
 months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 
@@ -30,6 +40,7 @@ for post in dates:
         url = base_url + post[0] + '/' + post[1]
         urls.append(url)
 
+print('\ngetting links to blog posts...')
 links = []
 for url in urls:
     print (url)
@@ -40,4 +51,16 @@ for url in urls:
     a_tag_matches = soup.select('h3.post-title.entry-title > a[href]')
     links += [a['href'] for a in a_tag_matches]
 
-print (links)
+print('\nscraping blog posts...')
+for link in links:
+    print (link)
+    response = requests.get(url)
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    
+    dl_links = soup.select('div.post-body.entry-content > div.separator > a[href*=/od.lk/f/]')
+    if len(dl_links) > 0:
+        dl_link = dl_links[0]['href']
+        zip_link = dl_link.replace('/f/', '/download/') + '?'
+        path = pwd + '/' + re.search(r'/[0-9][0-9]/(.+)\.html', link).group(1)
+        downloadIt(zip_link, path)
